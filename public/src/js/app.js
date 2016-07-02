2c79e3e98b7a10fa624e97ee028defb5d1854da9
mxxa <mxxa@live.com>
2016-07-02 06:09:22 +0300

    Update app.js

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,24 +1,26 @@
 $(document).ready(function(){
 var car_id = 0;
-var newfirstName = null;
+
+//OPEN THE MODAL
  $('.open-modal').click(function(){
-
- car_id= $(this).val();
+  //getting id from button value attrbuite
+  car_id= $(this).val();
      
+   //route  
   $.get('edit'+'/'+car_id , function(data){
-$('.update').text('Save Changes');
-       $('#brand').val(data.Brand);
+         //placing the data into the modal
+          $('#brand').val(data.Brand);
           $('#model').val(data.model);
-           $('#year').val(data.production_year);
-           $('#myModal').modal('show');
+          $('#year').val(data.production_year);
+          $('#myModal').modal('show');
   });
 
   
   });
   
+  //updating the data
    $('.update').click(function(){
-
-  
+    //ajax call
    $.ajax({
          method : 'POST' , 
          url : urlEdit , 
@@ -35,21 +37,14 @@ $('.update').text('Save Changes');
   
   });
   
-  
-    
-
-
-  
  
-  
-  
-  
+//Open add new modal  
  $('.add-modal').click(function(){
  
  $('#myModal').modal('show');
-
-           
-  });
+ });
+ 
+ //inserting new Data
  $('.add').click(function(e){
         e.preventDefault(); 
      $.ajax({
@@ -62,14 +57,16 @@ $('.update').text('Save Changes');
              _token : token
          }
      }).done(function(msg){
+      //adding the data to the page dynamically 
           $("table" ).append("<tr id="+msg['new_id']+">\n\
-     <td id='carBrand"+msg['new_id']+"'>"+msg['new_brand']+"</td>\n\
-<td id='carModel"+msg['new_id']+"'>"+msg['new_model']+"</td>\n\
-<td id='carYear"+msg['new_id']+"'>"+msg['new_year']+"</td>\n\
-  <td><button data-toggle='modal' data-target='#myModal' class='btn btn-xs btn-primary open-modal' value="+msg['new_id']+"><i class='glyphicon glyphicon-edit'></i> Edit</button></td>\n\
-<td><button class='btn btn-xs btn-danger delete' value="+msg['new_id']+"><i class='glyphicon glyphicon-trash'></i>Delete</button></td></tr>");
+                         <td id='carBrand"+msg['new_id']+"'>"+msg['new_brand']+"</td>\n\
+                         <td id='carModel"+msg['new_id']+"'>"+msg['new_model']+"</td>\n\
+                         <td id='carYear"+msg['new_id']+"'>"+msg['new_year']+"</td>\n\
+                         <td><button data-toggle='modal' data-target='#myModal' class='btn btn-xs btn-primary open-modal' value="+msg['new_id']+"><i class='glyphicon glyphicon-edit'></i> Edit</button></td>\n\
+                         <td><button class='btn btn-xs btn-danger delete' value="+msg['new_id']+">\n\
+                         <i class='glyphicon glyphicon-trash'></i>Delete</button></td></tr>");
       });
-            $('#myModal').modal('hide');      
+   $('#myModal').modal('hide');      
  setTimeout(function(){// wait for 5 secs(2)
            location.reload(); // then reload the page.(3)
       }, 500);
@@ -77,6 +74,7 @@ $('.update').text('Save Changes');
      
  });
   
+  //deleting data
      $('.delete').click(function(){
  car_id= $(this).val();
 
@@ -100,6 +98,8 @@ $('.update').text('Save Changes');
             
     
 });
+
+//clean the modal after closing it
   $('.close-modal').click(function(){
      
  
